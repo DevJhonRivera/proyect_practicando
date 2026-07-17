e821be7b99ff6f99da04dc5fb38716bbd7b87c84
DevJhonRivera <riverajhon052@gmail.com>
2026-07-17 08:54:02 -0500

    add cors

--- Back/src/middlewares/security.middleware.js
+++ Back/src/middlewares/security.middleware.js
@@ -15,6 +15,9 @@ export const corsOptions = {
     if (allowed.includes(origin)) {
       return callback(null, true);
     }
+    if (!origin) {
+  return callback(null, true);
+}
 
     return callback(new Error("Origen no permitido por CORS"));
   },
